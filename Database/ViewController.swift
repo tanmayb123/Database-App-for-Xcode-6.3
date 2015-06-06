4635b5888b037ee837d47a01cb8da404df5580bc
tanmayb123 <tajymany@gmail.com>
2015-06-06 11:13:10 -0400

    first commit

--- Database/ViewController.swift
+++ Database/ViewController.swift
@@ -1,25 +1,63 @@
 //
 //  ViewController.swift
-//  Database
+//  DatabaseTest
 //
-//  Created by Travis Barnett on 3/6/15.
-//  Copyright (c) 2015 Travis Barnett. All rights reserved.
+//  Created by Tanmay Bakshi on 2014-09-02.
+//  Copyright (c) 2014 TBSS. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+        @IBOutlet var tableview: UITableView!
+    
+        var data: NSArray = []
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            data = dataOfJson("www.mypgsite.com/travis/dataHandler.php")
+            println(data)
+            // Do any additional setup after loading the view, typically from a nib.
+        }
+    
+        @IBAction func reload() {
+            data = dataOfJson("www.mypgsite.com/travis/dataHandler.php")
+            self.tableview.reloadData()
+        }
+    
+        override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+            self.view.endEditing(true)
+        }
+    
+        func dataOfJson(url: String) -> NSArray {
+            var data = NSData(contentsOfURL: NSURL(string: url)!)
+            var error: NSError?
+            //var jsonArray: NSArray = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &error) as NSArray
+            //println(data)
+            return []
+        }
+    
+        func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            if data.count != 0 {
+                return data.count
+            } else {
+                return 1
+            }
+        }
+    
+        func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            var cell: additionInfoCell = self.tableview.dequeueReusableCellWithIdentifier("customCell") as additionInfoCell
+            var maindata = (data[indexPath.row] as NSDictionary)
+            cell.name!.text = maindata["First"] as String
+            cell.info!.text = maindata["Last"] as String
+            return cell
+        }
+    
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+    
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
-
--- Database/additionInfoCell.swift
+++ Database/additionInfoCell.swift
@@ -8,28 +8,12 @@
 
 import UIKit
 
-class additionInfoCell: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+class additionInfoCell: UITableViewCell {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
+    @IBOutlet var name: UILabel!
+    @IBOutlet var info: UITextView!
+    
 
 }
